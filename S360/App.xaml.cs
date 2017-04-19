44a6dcc5f5c88957ea60940564ee8b660809e26b
schoolmangement360 <schoolmangement360@outlook.com>
2017-04-19 22:27:12 +0530

    Custom Exception Class Library Added

--- S360/App.xaml.cs
+++ S360/App.xaml.cs
@@ -13,5 +13,34 @@ namespace S360
     /// </summary>
     public partial class App : Application
     {
+
+        public bool DoHandle { get; set; }
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (this.DoHandle)
+            {
+                //Handling And Logging the exception within the UnhandledExcpeiton handler.
+                S360Logging.S360Log.Instance.Error(e.Exception.Message, e);
+                e.Handled = true;
+            }
+            else
+            {
+                //If you do not set e.Handled to true, the application will close due to crash.
+                S360Logging.S360Log.Instance.Error("Uncaught Exception - Application is going to close ");
+                e.Handled = false;
+            }
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            S360Logging.S360Log.Instance.Error(ex, ex.Message);
+        }
+
     }
 }
---
+++ S360Exceptions/S360Exception.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S360Exceptions
+{
+    [Serializable]
+    public class S360Exception : Exception
+    {
+        public S360Exception()
+            : base() { S360Logging.S360Log.Instance.Error("Exception Occured"); }
+
+        public S360Exception(string message)
+            : base(message) { S360Logging.S360Log.Instance.Error(message); }
+
+        public S360Exception(string format, params object[] args)
+            : base(string.Format(format, args)) { S360Logging.S360Log.Instance.Error(format, args); }
+
+        public S360Exception(string message, Exception innerException)
+            : base(message, innerException) { S360Logging.S360Log.Instance.Error(innerException, message); }
+
+        public S360Exception(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { S360Logging.S360Log.Instance.Error(innerException, innerException.Message, args); }
+
+        protected S360Exception(SerializationInfo info, StreamingContext context)
+            : base(info, context) { S360Logging.S360Log.Instance.Error(info.ToString(), context); }
+
+        //////////// Sample For Throwing Excetions /////////
+        ////throw new S360Exception()
+        ////throw new S360Exception(message)
+        ////throw new S360Exception("Exception with parameter value '{0}'", param)
+        ////throw new S360Exception(message, innerException)
+        ////throw new S360Exception("Exception with parameter value '{0}'", innerException, param)
+    }
+}
