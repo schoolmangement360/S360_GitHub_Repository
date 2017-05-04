ee9318c36aef20a49423d8569ed70eff1b708378
schoolmangement360 <schoolmangement360@outlook.com>
2017-05-04 21:24:28 +0530

    Removed function

--- S360/ViewModel/Student/StudentViewModel.cs
+++ S360/ViewModel/Student/StudentViewModel.cs
@@ -713,26 +713,6 @@ namespace S360
             return new ObservableCollection<GEN_Standards_Lookup>(Standards);
         }
 
-        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
-        {
-            if (depObj != null)
-            {
-                for (int i = 0; i < System.Windows.Media.VisualTreeHelper.GetChildrenCount(depObj); i++)
-                {
-                    DependencyObject child = System.Windows.Media.VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null && child is T)
-                    {
-                        yield return (T)child;
-                    }
-
-                    foreach (T childOfChild in FindVisualChildren<T>(child))
-                    {
-                        yield return childOfChild;
-                    }
-                }
-            }
-        }
-
         #endregion
     }
 }
