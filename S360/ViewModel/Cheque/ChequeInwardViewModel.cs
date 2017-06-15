afcacb28c4322511921cd2766dc886332e2a170a
schoolmangement360 <schoolmangement360@outlook.com>
2017-06-15 23:15:10 +0530

    Cheque Inwards Design completed without student add popup

---
+++ S360/ViewModel/Cheque/ChequeInwardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S360.ViewModel.Cheque
+{
+    public class ChequeInwardViewModel : ViewModelBase
+    {
+        #region [ Private Variables ]
+
+        #endregion
+
+        #region [ Constructor ]
+
+        #endregion
+
+        #region [ Public Properties ]
+
+        #endregion
+
+        #region [ Events ]
+
+        #endregion
+    }
+}
